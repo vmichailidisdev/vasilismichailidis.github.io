51a254a9c1d942b1b3135e2b356375024b543b0e
Vasileios Michailidis Spanoudis <vasilis@pop-os.localdomain>
2021-08-28 22:26:05 +0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+;(function () {
+  window.addEventListener('load', function () {
+    let mainContent = document.getElementsByClassName('main-content')[0]
+    mainContent.classList.add('main-content--animation')
+  })
+})()
