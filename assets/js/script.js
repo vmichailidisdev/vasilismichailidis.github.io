523a4317d3241e26c2bb56eb26cc7bada8d95900
Vasilis Michailidis <vasilismichailidisdev@gmail.com>
2021-08-29 15:05:12 +0300

    Fix image scaling, add page loader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 ;(function () {
   window.addEventListener('load', function () {
+    let loader = document.getElementsByClassName('loader')[0]
+    loader.classList.add('loader--hide')
     let mainContent = document.getElementsByClassName('main-content')[0]
     mainContent.classList.add('main-content--animation')
   })
